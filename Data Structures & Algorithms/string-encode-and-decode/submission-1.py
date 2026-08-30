ff18f45ec2d7b514aff4c1ca61fd125507417b0d
edward <124009309+Edward201323@users.noreply.github.com>
2026-08-29 17:28:04 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        sol = ""
+        for s in strs:
+            sol = sol + str(len(s)) + '%' + s
+        return sol
+
+
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 0:
+            return []
+        
+        sol = []
+
+        i = 0
+        while i < len(s):
+            j = i
+            s_length = ""
+            while s[j] != '%':
+                s_length += str(s[j])
+                j += 1
+            j += 1
+
+            s_length = int(s_length)
+
+            sol_i = ""
+            while s_length > 0:
+                sol_i += s[j]
+                s_length -= 1
+                j += 1
+            
+            sol.append(sol_i)
+            i = j
+        
+        return sol
