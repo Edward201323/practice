5c2758475b4c78e8641512ecfba629af23beac04
edward <124009309+Edward201323@users.noreply.github.com>
2026-09-09 18:10:20 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        sol = [0] * len(temperatures)
+        s = []
+        for i in range(0, len(temperatures)):
+            curr = temperatures[i]
+            while len(s) > 0:
+                peek_index = s[len(s) - 1]
+                peek_value = temperatures[peek_index]
+                if curr > peek_value:
+                    sol[peek_index] = i - peek_index
+                else:
+                    break
+                s.pop()
+            
+            s.append(i)
+        
+        for i in s:
+            sol[i] = 0
+
+        return sol
+                
